9685acbc2d2c294fb279c57f0caab39041b8f5aa
jacobclapper <clapperpianist@gmail.com>
2022-06-11 14:36:18 -0400

    Every click will alternate between marking an X and O

--- index.js
+++ index.js
@@ -17,18 +17,52 @@ const div8 = document.querySelector('#eight')
 const div9 = document.querySelector('#nine')
 
 
-// change innerText of div click
- function updateDiv1() { 
-     div1.style.backgroundColor = "blue";
+// change innercolor of div click
+function updateDiv1() { 
+     gamePlay()
+     div1.style.backgroundColor = color;
    }
-const updateDiv2 = () => div2.style.backgroundColor = "blue"//this should be a variable that changes when clicked by iterator // then gameflow iterater += 1
-const updateDiv3 = () => div3.style.backgroundColor = "blue"
-const updateDiv4 = () => div4.style.backgroundColor = "blue"
-const updateDiv5 = () => div5.style.backgroundColor = "blue"
-const updateDiv6 = () => div6.style.backgroundColor = "blue"
-const updateDiv7 = () => div7.style.backgroundColor = "blue"
-const updateDiv8 = () => div8.style.backgroundColor = "blue"
-const updateDiv9 = () => div9.style.backgroundColor = "blue"
+function updateDiv2() { 
+    gamePlay()
+    div2.style.backgroundColor = color;
+  }
+function updateDiv3() { 
+    gamePlay()
+    div3.style.backgroundColor = color;
+  }
+  function updateDiv4() { 
+    gamePlay()
+    div4.style.backgroundColor = color;
+  }
+  function updateDiv5() { 
+    gamePlay()
+    div5.style.backgroundColor = color;
+  } 
+  function updateDiv6() { 
+    gamePlay()
+    div6.style.backgroundColor = color;
+  }
+  function updateDiv7() { 
+    gamePlay()
+    div7.style.backgroundColor = color;
+  }
+  function updateDiv8() { 
+    gamePlay()
+    div8.style.backgroundColor = color;
+  }
+  function updateDiv9() { 
+    gamePlay()
+    div9.style.backgroundColor = color;
+  }
+
+//const updateDiv2 = () => gameplay(); div2.style.backgroundColor = color;//this should be a variable that changes when clicked by iterator // then gameflow iterater += 1
+//const updateDiv3 = () => div3.style.backgroundColor = "blue"
+//const updateDiv4 = () => div4.style.backgroundColor = "blue"
+//const updateDiv5 = () => div5.style.backgroundColor = "blue"
+//const updateDiv6 = () => div6.style.backgroundColor = "blue"
+//const updateDiv7 = () => div7.style.backgroundColor = "blue"
+//const updateDiv8 = () => div8.style.backgroundColor = "blue"
+//const updateDiv9 = () => div9.style.backgroundColor = "blue"
 
 //   div1.addEventListener('click', function (event) {
 //   } ) 
@@ -46,7 +80,24 @@ div9.addEventListener('click', updateDiv9)
 //}
 
 // * Every click will alternate between marking an `X` and `O`
-const gameflow = ["playX", "playO","playX", "playO","playX", "playO","playX", "playO","playX"]
+const gameFlow = ["playX", "playO","playX", "playO","playX", "playO","playX", "playO","playX"]
+let turn = 0
+console.log(gameFlow[turn])
+function gamePlay(){
+    //if last move and no winner, then gamestatus is draw
+    if (gameFlow[turn] === gameFlow.length && gameStatus != gamestatus) {
+        gameStatus = gameStatus[2]
+    }
+    //change color according to turn
+    if (gameFlow[turn] === "playX") {
+        console.log(gameFlow[turn])
+        color = "red" 
+    } else if (gameFlow[turn]){
+        console.log(gameFlow[turn])
+        color = "blue"
+    }
+    return turn += 1
+}
 
 function checkIfWinner() {
     //if row one is same === winner
@@ -59,16 +110,16 @@ function checkIfWinner() {
     //if diagonal two is same === winner
 }
 
-
 // create variable to track which turn it is
 // while gamestatus does not equal win or draw, check if win
 const gameStatus = ['play', 'win', 'draw']
-gameStatus = gameStatus[0]
-
+//gameStatus = gameStatus[0]
 const win = 'win'
+// if 
 const draw = "draw"
+// if draw- change display message
+let color = null;
 
-let playersTurn = null;
     // inner content will be while loop
 // while game is not win, for loop playes turn = gameflow[i]
 //   while ( gameStatus !== gameStatus[1] || gameStatus[2]) {
@@ -112,10 +163,10 @@ let playersTurn = null;
 
 // add an event listener to my generate button
 const restButton = document.querySelector("reset-button")
-restButton.addEventListener('click', restGame)
+// restButton.addEventListener('click', restGame)
 
-function restGame() {
-    //changle player turn, turn counter, display message
-}
+// function restGame() {
+//     //changle player turn, turn counter, display message
+// }
 
 //})
